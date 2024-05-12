6057ee22fa6acfdbfb9281bc53a35ab56c5c9038
Henrique Torres <henriquetorrescampos@gmail.com>
2024-05-12 15:43:19 -0300

    methods for dictionaries

---
+++ dictionaries/class_121.py
@@ -0,0 +1,21 @@
+"""
+Useful methods for dictionaries
+"""
+
+people = {
+    'name': 'Luiz Otavio',
+    'surname': 'Miranda',
+    'age': 25,
+}
+
+print(len(people)) # return quantity of keys
+print(people.keys()) # return keys of dictionary
+print(people.values()) # return values of dictionary keys
+print(people.items()) # return key and value
+print(people.setdefault()) # add value to a key doesn't exist
+
+# for value in people.values():
+#     print(value)
+
+# for key, value in people.items():
+#     print(key, value)
