074c18aed1d999eefa4fb7ee1975d6549059ffb1
Henrique Torres <henriquetorrescampos@gmail.com>
2024-05-13 20:34:14 -0300

    new tests with none as False

---
+++ exercicios/dictionary_class124.py
@@ -0,0 +1,20 @@
+questions = [
+    {
+        'question': 'How much is 2+2?',
+        'options': ['1', '3', '4', '5'],
+        'answers': '4',
+    },
+    {
+        'question': 'How much is 5*5?',
+        'options': ['25', '55', '10', '51'],
+        'answers': '25',
+    },
+    {
+        'question': 'How much is 10/2?',
+        'options': ['5', '5', '2', '1'],
+        'answers': '5',
+    },
+]
+
+for question in questions:
+    print(question['question'])
